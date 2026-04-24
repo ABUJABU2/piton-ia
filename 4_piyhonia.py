de585f48c1fc81b7541257809112bb975003fb8d
ABUJABU2 <brandojoao86@gmail.com>
2026-04-24 16:09:28 -0300

    Add files via upload

--- 4_piyhonia.py
+++ 4_piyhonia.py
@@ -16,7 +16,7 @@ load_dotenv()
 # ETAPA 1 — Carregar e formatar o Excel
 # ============================================================
 df = pd.read_excel("pergunta_resposta.xlsx", header=None) #criamos uma tabela que lê o meu exel
-df.columns = ["pegunta", "resposta"]
+df.columns = ["pergunta", "resposta"]
 
 docs_formatados = [
     Document(
@@ -38,3 +38,59 @@ splitter = RecursiveCharacterTextSplitter(
 
 chunks=splitter.split_documents(docs_formatados)
 print(f'chunks gerados:{len(chunks)}')
+
+# ============================================================
+# ETAPA 3 —embeddingsV 
+# ============================================================
+
+embedddings = OpenAIEmbeddings(model='text-embedding-3-small')
+
+# ============================================================
+# ETAPA 4 —vector store 
+# ============================================================
+
+vector_store = FAISS.from_documents(chunks,embedddings) # originei a vector store usando os chunks gerados e o modelo de embedding 
+#especificados
+vector_store.save_local('minha_vectorstore')
+print('vector store foi salva em disco local')
+
+# ============================================================
+# ETAPA 5 —prompt + llm
+# ============================================================
+
+llm=ChatOpenAI(model="gpt-4o-mini",temperature = 0)
+prompt = ChatPromptTemplate.from_template(
+    """você é um assistente prestativo que deverá responder perguntas exclusivamente com base no contexto abaixo
+    .caso vocênão saiba a resposta, simplesmente fale que mâo sabe a resposta
+
+    contexto:{context}
+    
+    pergunta:{input}
+
+    """
+
+)
+
+# ============================================================
+# ETAPA 6 — Cadeia RAG via LCEL
+# ============================================================
+
+retriever = vector_store.as_retriever(search_kwargs={"k": 3}) # pegando 3 chunks da minha vectore store
+
+def formatar_docs(docs): #formatando a schunks
+    return "\n\n".join(doc.page_content for doc in docs)
+
+#uma linguagem de encadeiamento onde os estágios se conectam
+qa_chain = (
+    {"context": retriever  | formatar_docs , 'input':RunnablePassthrough()} # alimento o meu contexto formatando as chunks selecionadas no retriever
+    |prompt #usamos o chat promt template e alimentamos o {contex} do prompt
+    | llm #enviamos o prompt para a llm destacada
+    |StrOutputParser () # formatamos a resposta da llm
+)
+
+
+pergunta = 'por que o vitor faltou no galileu hoje?'
+print(f'\nPergunta: {pergunta}')
+
+resposta = qa_chain.invoke(pergunta)
+print(f'\nResposta: {resposta}')
